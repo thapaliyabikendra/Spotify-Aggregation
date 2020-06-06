f6ef3e7ba67566c06242a1294db301a21654b92d
Bikendra Thapaliya <thapaliyabikendra@gmail.com>
2020-06-06 12:22:05 +0545

    Initial

---
+++ src/artist.py
@@ -0,0 +1,32 @@
+# Import modules 
+
+import pandas as pd
+from ast import literal_eval
+import json
+from spotify import Spotify
+
+class Artist(Spotify):
+    def __init__(self):
+        self.load_data()
+        self.cast_to_list()
+
+    def load_data(self, filename="data/data_w_genres.csv"):
+        self.data = pd.read_csv(filename)
+
+    def cast_to_list(self):
+        self.data['genres'] = self.data.genres.apply(lambda x: literal_eval(x))
+
+    def check_for_item(self, x, item='rap'):
+        for i in x:
+            if item in i:
+                 return True
+        return False
+
+    def sort_by_popularity(self, filtered_data):
+        return filtered_data.sort_values('popularity').reset_index(drop=True)
+    
+    def to_json(self, sorted_data):
+        return json.loads(sorted_data.to_json(orient='records'))
+
+    def artists_by_genre(self, genre):
+        return self.to_json(self.sort_by_popularity(self.data[self.data.genres.apply(self.check_for_item, item=genre)]))
---
+++ src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import uvicorn
+from song import Song
+from artist import Artist
+
+# init
+app = FastAPI(debug=True)
+
+song = Song()
+artist = Artist()
+
+# Route
+@app.get("/")
+def say_hello():
+    return {"Welcome":"Spotify Aggergation"}
+
+@app.get("/artist/{artist}")
+def get_songs_by_artist(artist:str, page:int = 0, size:int=10):
+    return song.songs_by_artist(artist)[page*size:(page+1)*size]
+
+@app.get("/year/{year}")
+def get_songs_by_year(year:int, page:int = 0, size:int=10):
+    return song.songs_by_year(year)[page*size:(page+1)*size]
+
+@app.get("/genre/{genre}")
+def get_artist_by_genre(genre:str, page:int = 0,size:int=10):
+    return artist.artists_by_genre(genre)[page*size:(page+1)*size]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port = "8000")
